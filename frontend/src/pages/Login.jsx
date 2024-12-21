0d348f0a107a818966c5a228b1cb82992f59bed0
Srinjoy Das <96923080+srinjoy29@users.noreply.github.com>
2024-12-21 19:29:25 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -30,8 +30,8 @@ const Login = () => {
   }
   return (
     <>
-    <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
-    <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">Blog Market</Link></h1>
+    <div className="bg-pink-500 flex items-center justify-between px-6 md:px-[200px] py-4">
+    <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">Tech Blog</Link></h1>
     <h3><Link to="/register">Register</Link></h3>
     </div>
 <div className="w-full flex justify-center items-center h-[80vh] ">
@@ -39,11 +39,11 @@ const Login = () => {
          <h1 className="text-xl font-bold text-left">Log in to your account</h1>
          <input onChange={(e)=>setEmail(e.target.value)} className="w-full px-4 py-2 border-2 border-black outline-0" type="text" placeholder="Enter your email" />
          <input onChange={(e)=>setPassword(e.target.value)} className="w-full px-4 py-2 border-2 border-black outline-0" type="password" placeholder="Enter your password" />
-         <button onClick={handleLogin} className="w-full px-4 py-4 text-lg font-bold text-white bg-black rounded-lg hover:bg-gray-500 hover:text-black ">Log in</button>
+         <button onClick={handleLogin} className="w-full px-4 py-4 text-lg font-bold text-white bg-pink-500 rounded-lg hover:bg-red-500 hover:text-black ">Log in</button>
          {error && <h3 className="text-red-500 text-sm ">Something went wrong</h3>}
          <div className="flex justify-center items-center space-x-3">
           <p>New here?</p>
-          <p className="text-gray-500 hover:text-black"><Link to="/register">Register</Link></p>
+          <p className="text-gray-500 hover: text-black"><Link to="/register">Register</Link></p>
          </div>
        </div>
     </div>
