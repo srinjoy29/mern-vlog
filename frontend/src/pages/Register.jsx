a9f29b6a4447f61f5a6cdab254c3e12a889cfdea
Srinjoy Das <96923080+srinjoy29@users.noreply.github.com>
2024-12-21 19:30:30 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -35,8 +35,8 @@ const Register = () => {
 
   return (
     <>
-      <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
-    <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">Blog Market</Link></h1>
+      <div className="bg-pink-500 flex items-center justify-between px-6 md:px-[200px] py-4">
+    <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">Tech Blog</Link></h1>
     <h3><Link to="/login">Login</Link></h3>
     </div>
     <div className="w-full flex justify-center items-center h-[80vh] ">
